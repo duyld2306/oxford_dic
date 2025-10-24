ea6613ef6f8483937f93494e772f652dc2783862
Duy <you@example.com>
2025-10-25 02:53:54 +0700

    comiit

--- src/routes/importRoutes.js
+++ src/routes/importRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import ImportController from "../controllers/ImportController.js";
 import asyncHandler from "../middleware/asyncHandler.js";
-import { adminMiddleware } from "../middleware/authMiddleware.js";
+import authMiddleware, {
+  adminMiddleware,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const importController = new ImportController();
+
+// All flashcard group routes require authentication
+router.use(authMiddleware);
 
 router.post(
   "/json",
